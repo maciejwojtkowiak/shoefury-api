d427dc67db2d93375a1ed00f569660282b20535f
maciejwojtkowiak <mac.woj1000@gmail.com>
2022-10-16 19:22:09 +0200

    output product title correctly

--- controllers/profile.ts
+++ controllers/profile.ts
@@ -7,6 +7,7 @@ import { getUser } from "../utils/user/getUser";
 import PDFDocument from "pdfkit";
 import fs from "fs";
 import path from "path";
+import { IProduct } from "../types/Product";
 
 export const getProfile = async (
   req: Request<{}, {}, IAuthUser>,
@@ -14,6 +15,8 @@ export const getProfile = async (
   next: NextFunction
 ): Promise<void> => {
   const currentUser = await getUser(req.body.userId);
+  console.log("USERID", req.body.userId);
+  console.log("CURRENT USER", currentUser);
   if (currentUser != null) {
     const userOrders = await currentUser.populate("orders.order", "totalPrice");
     res.status(200).json({
@@ -31,7 +34,9 @@ export const getOrderRaport = async (
   next: NextFunction
 ): Promise<void> => {
   const orderId = req.params.orderId;
-  const order = await Order.findOne({ orderId });
+  const order = await Order.findOne({ orderId: "634c361e6f30118f1228307a" });
+  console.log("ORDERID", orderId);
+  console.log("ACUTAL ORDERID", order);
   if (order === null) {
     next(createError("Order does not exist"));
     return;
@@ -51,16 +56,19 @@ export const getOrderRaport = async (
   }
 
   const pdfName = `cv123${orderId}.pdf`;
-  res.setHeader("Content-Type", "application/pdf");
+  res.setHeader("Content-Type", "text/pdf");
   res.setHeader("Content-Disposition", `attachment; filename=${pdfName}`);
-  const products = await order.populate("items");
-  console.log(products.items, "ITEMS");
+  const products = await order.populate("items.product");
   const pdfPath = path.join("data", pdfName);
   const pdfStream = fs.createWriteStream(pdfPath);
   const pdfDoc = new PDFDocument();
   const writeStream = pdfDoc.pipe(pdfStream);
   pdfDoc.fontSize(25).text(`Order ${orderId}`);
   pdfDoc.fontSize(13).text(`User ${orderUser.name}`);
+  console.log("PRODUCTS", products);
+  products.items.forEach((p) => {
+    pdfDoc.fontSize(14).text((p.product as unknown as IProduct).title);
+  });
 
   pdfDoc.end();
   writeStream.on("finish", () => {
